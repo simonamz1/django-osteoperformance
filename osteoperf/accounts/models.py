17ecbe998a4ede32c615094694b0f1d8e8c6068e
simonamz <simonamzalag@gmail.com>
2018-02-16 18:34:02 +0100

    implementing the Customized User Model, and proceed to the migration

--- osteoperf/accounts/models.py
+++ osteoperf/accounts/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, first_name, last_name, dob, password=None):
+        if not email:
+            raise ValueError("Email is mandatory.")
+        user = self.models(
+            email = self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            dob=dob,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, last_name, dob, password=None):
+        user = self.create_user(email, first_name, last_name, dob, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=40, verbose_name="First Name")
+    last_name = models.CharField(max_length=40, verbose_name="Last Name")
+    dob = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "dob", "password"]
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
